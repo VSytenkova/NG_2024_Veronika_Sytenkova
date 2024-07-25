66f4650007d73da59d2306e5b5906603e2c9ac7f
Veronika-Sytenkova-KN-23 <146567625+VSytenkova@users.noreply.github.com>
2024-07-26 02:22:47 +0300

    Update task 1.6

--- Lection_1/task_6/main.cpp
+++ Lection_1/task_6/main.cpp
@@ -2,25 +2,11 @@
 
 using namespace std;
 
-/*void drawPine(int size, int space){
-    if (size > 1){
-        drawPine(size-1, space+1);
-    }
-    int doubleStar = size * 2 - 1;
-    for (int i = 0; i < space; i++)
-        cout << " ";
-    for (int i = 0; i < doubleStar; i++)
-        cout << "*";
-    cout << endl;
-}*/
-
 int main()
 {
     int size;
     cout << "Enter size of pine: " << endl;
     cin >> size;
-
-    //drawPine(size, 0);
 
     int space = size;
     for(int j = 0; j < size; j++){
