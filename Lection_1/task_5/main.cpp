620082203b91d4961f26c8ceafa96f0301265e15
Veronika <kin77444@gmail.com>
2024-08-03 23:05:13 +0300

    Fixed task 5

--- Lection_1/task_5/main.cpp
+++ Lection_1/task_5/main.cpp
@@ -5,24 +5,23 @@ using namespace std;
 
 int main()
 {
-    int numberA, numberB, numberC;
-    float discriminant, rootFirst, rootSecond;
-    cout << "Enter numberA: " << endl;
+    float numberA, numberB, numberC, discriminant, rootFirst, rootSecond;
+    cout << "Enter a: " << endl;
     cin >> numberA;
-    cout << "Enter numberB: " << endl;
+    cout << "Enter b: " << endl;
     cin >> numberB;
-    cout << "Enter numberC: " << endl;
+    cout << "Enter c: " << endl;
     cin >> numberC;
 
-    discriminant = (numberB * numberB - 4 * numberA * numberC);
+    discriminant = (numberB * numberB - (4 * numberA * numberC));
     if (discriminant < 0){
-        rootFirst = (- numberB ) / 2 * numberA;
-        rootSecond = (sqrt(-discriminant)) / 2 * numberA;
+        rootFirst = (- numberB ) / (2 * numberA);
+        rootSecond = (sqrt(-discriminant)) / (2 * numberA);
         cout << "Result:\nx1 = " << rootFirst << " + " << rootSecond << "i\nx2 = " << rootFirst << " - " << rootSecond << "i";
         return 0;
     } else {
-        rootFirst = (- numberB - sqrt(discriminant)) / 2 * numberA;
-        rootSecond = (- numberB + sqrt(discriminant)) / 2 * numberA;
+        rootFirst = (- numberB - sqrt(discriminant)) / (2 * numberA);
+        rootSecond = (- numberB + sqrt(discriminant)) / (2 * numberA);
     }
 
     if (rootFirst == rootSecond){
